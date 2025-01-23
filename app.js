32dbc2c2c5ad8f13841d55287c69fa13738a928b
teresarey8 <teresareyagujetas@gmail.com>
2025-01-23 18:24:52 +0000

    11.b.3

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('/', (req,res)=>{
+    let oferta = req.cookies.preferencia;
+    res.render('index',{oferta});
+})
+
+app.post('/anuncio', (req, res) => {
+    let preferencia = req.body.preferencia;
+    res.cookie('preferencia', preferencia, { maxAge: 24 * 60 * 60 * 1000 }); 
+})
+
+app.listen(8001);
